3e14ff0165ccb49a66393fcbea041a11464f21e8
hparadox82 <aust.i.mo34@gmail.com>
2024-09-30 18:52:40 -0400

    uhh,,,, maff.... ouch

--- Ch3 Project/Source.cpp
+++ Ch3 Project/Source.cpp
@@ -17,11 +17,28 @@ Do NOT use system(pause).
 */
 
 #include <iostream>
-#include <iomanip>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main()
 {
+	const int MIN_VAL = 100, MAX_VAL = 1000;
 
-	return 0;
+	int random1, random2, answer;
+
+	unsigned seed = time(0);
+	srand(seed);
+
+	cout << "Welcome to the super mega Tutor-tron MK IV.\n\nHere's a random math problem for you to practice on!" << endl;
+	random1 = (rand() % (MAX_VAL - MIN_VAL + 1)) + MIN_VAL;
+	random2 = (rand() % (MAX_VAL - MIN_VAL + 1)) + MIN_VAL;
+
+	answer = random1 + random2;
+
+	cout << "\n" << random1 << "+" << random2 << "= ? \n" << endl;
+	cout << "Try to figure out this problem. To check your answer, press ENTER to see the solution." << endl;
+	cin.get();
+	cout << random1 << "+" << random2 << "=" << answer << endl;
+		return 0;
 }
