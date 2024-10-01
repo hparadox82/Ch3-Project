ac96a5fe33199553c02473c699298f083309ef1b
hparadox82 <aust.i.mo34@gmail.com>
2024-09-30 20:40:28 -0400

    first push, works

--- Ch3 Project/Source.cpp
+++ Ch3 Project/Source.cpp
@@ -2,34 +2,34 @@
 //CSC 221 63H FA24
 //Instructor Andrew Sprague
 //Austin Mollard
-/*Project 1. Ingredient Adjuster
-A cookie recipe calls for the following ingredients:
- 1.5 cups of sugar
- 1 cup of butter
- 2.75 cups of flour
-The recipe produces 48 cookies with this amount of ingredients. Write a program that asks the user how
-many cookies he or she wants to make, then displays the number of cups of each ingredient needed for
-the specified number of cookies*/
+/*Assuming there are no deposits other than the original investment, the balance in a
+savings account after one year may be calculated as amount = principle * (1+rate/T)^T.
+Principal is the balance in the savings account, Rate is the interest rate, and T is
+the number of times the interest is compounded during a year (T is 4 if the interest
+is compounded quarterly).
+Write a program that asks for the principal, the interest rate, and the number of
+times the interest is compounded.*/
 //updated to do the branch thing
 
 #include <iostream>
 #include <iomanip>
+#include <cmath>
+
 using namespace std;
 
 int main()
 {
-	const double cuppAsug = 1.5, cuppAbutt = 1.0, cuppAflou = 2.75;
-	double resultSug, resultButt, resultFlour, ingredMult;
-	const int cookiesOrig = 48;
-	int cookiesDesired;
+	float amount, princi, numsComp, inRate, totalinter;
 
-	cout << "You need 1.5 cups of sugar, 1 cup of butter and 2.75 cups of flour to make 48 cookies.\nHow many cookies would you like to bake?" << endl;
-	cin >> cookiesDesired;
-	ingredMult = static_cast<double>(cookiesDesired) / cookiesOrig;
-	resultSug = cuppAsug * ingredMult;
-	resultButt = cuppAbutt * ingredMult;
-	resultFlour = cuppAflou * ingredMult;
-	cout << "To make " << cookiesDesired << " cookies, you'll need:\n" << std::setprecision(3) << resultSug << " cups of sugar," << resultButt << " cups of butter, and " << resultFlour << " cups of flour." << endl;
-
+	cout << "Welcome to the interest earned calculator.\nThis calculates your total after one year.\nWhen prompted, please input your inputs to be inputted." << endl;
+	cout << "Interest rate (in decimal form, e.g. 4.25% > .0425)?" << endl;
+	cin >> inRate;
+	cout << "How many times compounded a year?" << endl;
+	cin >> numsComp;
+	cout << "Your principal?" << endl;
+	cin >> princi;
+	amount = princi * pow((1 + inRate / numsComp), numsComp);
+	totalinter = amount - princi;
+	cout << "\n\n\nInterest rate: " << inRate << "\n" << "Times compounded: " << numsComp << "\n" << "Principal: " << princi << "\n" << "Interest: " << totalinter << "\n" << "Total amount after one year: " << amount << endl;
 	return 0;
 }
